e8680cf3f996d16f00ecd7419a46e5d71b36ca81
Vineeta <vineeta.1223@gmail.com>
2020-11-21 18:44:11 +0530

    changed background

--- sketch.js
+++ sketch.js
@@ -38,7 +38,7 @@ function setup(){
 }
 
 function draw(){
-    background(backgroundImg);
+    background("green");
     Engine.update(engine);
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
